894dedb386d91864d747d6a653aff5a540f41847
masud-almahdi-dev <functionnotworkingcorrectly@gmail.com>
2023-12-12 17:28:05 +0600

    started project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <nav className='bg-pink-500 text-white'>
+        Navigation
+      </nav>
+      <section className='bg-green-600 text-white'>
+        Section1
+      </section>
+      <footer className='bg-blue-500 text-white'>
+        Footer
+      </footer>
+    </>
+  )
+}
+
+export default App
